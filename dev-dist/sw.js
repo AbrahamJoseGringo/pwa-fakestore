fb60a253fa03c775472c026e7aca59d63454365b
AbrahamJoseGringo <aperez.castro32@gmail.com>
2024-07-24 15:35:17 -0300

    Burro né abraham

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8gj51b3dj3g"
+    "revision": "0.os0v37p5jd8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
